8b3edc4300cbb9053685295652dcc82f3b1a038b
Amjad <amjadali26907@gmail.com>
2026-09-22 18:04:46 +0530

    chore: exclude SRS, documentation, generation scripts, and media; make run.bat portable

--- run.bat
+++ run.bat
@@ -2,9 +2,9 @@
 title Launch Project
 
 :: Start server in a new window
-start "Server" cmd /k "cd /d C:\Users\notam\Desktop\I_hate_SIH\server && npm run dev"
+start "Server" cmd /k "cd /d "%~dp0server" && npm run dev"
 
 :: Start client in a new window
-start "Client" cmd /k "cd /d C:\Users\notam\Desktop\I_hate_SIH\client && npm run dev"
+start "Client" cmd /k "cd /d "%~dp0client" && npm run dev"
 
 exit
